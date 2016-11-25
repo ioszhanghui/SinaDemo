d6f9200a38d52ed57802c4ca4eb4c5a30c69bdfe
xiaofeiniao <ioszhanghui@163.com>
2016-11-25 11:18:04 +0800

    首次提交

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SinaDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SinaDemo
+
+  pod 'SDWebImage', '~> 3.7'
+
+  target 'SinaDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SinaDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ SinaDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SinaDemo
+//
+//  Created by zhph on 2016/11/25.
+//  Copyright © 2016年 zhph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor=UIColor.red;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
